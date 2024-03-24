e79d99d7590517a2c172bf0080887104f6e5333a
IdAle3 <idalia.delgadom@uanl.edu.mx>
2024-03-23 20:53:51 -0600

    Ciclo de vida de un componente

--- miApp-2132987/src/app/home/home.page.ts
+++ miApp-2132987/src/app/home/home.page.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage{
+constructor(){
+  console.log("AppComponent: Constructor");
+}
+ngOnChanges(){
+  console.log("AppComponent: OnChanges");
+}
 
-  constructor() {}
+ngOnInit(){
+  console.log("AppComponent: OnInit");
+}
+
+ngDoCheck(){
+  console.log("AppComponent: DoCheck");
+}
+
+ngAfterContentInit(){
+  console.log("AppComponent: AfterContentInit");
+}
+
+ngAfterContentChecked(){
+  console.log("AppComponent: AfterContentChecked");
+}
+
+ngAfterViewInit(){
+  console.log("AppComponent: AfterViewInit");
+}
+
+ngAfterViewChecked(){
+  console.log("AppComponent: AfterContentChecked");
+}
+
+ngOnDestroy(){
+  console.log("AppComponent: OnDestroy");
+} 
 
 }
